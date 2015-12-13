190bef3496a80b986e286d828b5ac73e05d6205c
Cerdic <cedric@yterium.com>
2015-12-13 11:53:58 +0000

    Indentation et regles de codage selon http://www.spip.net/fr_article3497.html#regles_codage

--- inc/safehtml.php
+++ inc/safehtml.php
@@ -11,7 +11,9 @@
 \***************************************************************************/
 
 
-if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) {
+	return;
+}
 
 // Controle la presence de la lib safehtml et cree la fonction
 // de transformation du texte qui l'exploite
@@ -22,15 +24,17 @@ function inc_safehtml_dist($t) {
 	if (!$test) {
 		$process = false;
 		if ($f = find_in_path('lib/safehtml/classes')) {
-			define('XML_HTMLSAX3', $f.'/');
-			require_once XML_HTMLSAX3.'safehtml.php';
+			define('XML_HTMLSAX3', $f . '/');
+			require_once XML_HTMLSAX3 . 'safehtml.php';
 			$process = new safehtml();
 			$process->deleteTags[] = 'param'; // sinon bug Firefox
 		}
-		if ($process)
-			$test = 1; # ok
-		else
-			$test = -1; # se rabattre sur une fonction de securite basique
+		if ($process) {
+			$test = 1;
+		} # ok
+		else {
+			$test = -1;
+		} # se rabattre sur une fonction de securite basique
 	}
 
 	if ($test > 0) {
@@ -45,13 +49,13 @@ function inc_safehtml_dist($t) {
 #		$process->parse(''); # cas particulier ?
 		$process->clear();
 		$t = $process->parse($t);
-	}
-	else
-		$t = entites_html($t); // tres laid, en cas d'erreur
+	} else {
+		$t = entites_html($t);
+	} // tres laid, en cas d'erreur
 
 	// supprimer un <li></li> provenant d'un <li> ouvrant seul+safehtml
 	// cf https://core.spip.net/issues/2201
-	$t = str_replace("<li></li>","",$t);
+	$t = str_replace("<li></li>", "", $t);
 
 	return $t;
 }
