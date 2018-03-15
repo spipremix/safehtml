48a9cc47ed3b640d17d39d6cdd871c9af79f53bb
gpl <gpl@long-player.net>
2018-03-15 04:42:11 +0000

    [Salvatore] [source:_core_/plugins/safehtml/lang/ paquet-safehtml] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-safehtml_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-safehtml?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'safehtml_description' => 'Ŝirmilo por la forumoj kontraŭ transreteja atakoj',
+	'safehtml_slogan' => 'Ŝirmilo por la forumoj kontraŭ transreteja atakoj'
+);
